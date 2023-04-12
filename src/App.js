83d9b294e3baf9e3f5a69ca92b69c3a78ff90ce1
Andy Li <shadowlpk05@gmail.com>
2023-04-11 21:27:08 -0700

    add router, board and homepage components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.scss';
+import "./App.scss";
+import {BrowserRouter, Routes , Route} from "react-router-dom"
+import HomePage from "./components/HomePage/HomePage";
+import Board from "./components/Board/Board";
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/arena" element={<Board />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
