690030b2b4f25e7ff55712497e44bb7b4cfa265d
Andy Li <shadowlpk05@gmail.com>
2023-04-12 09:55:21 -0700

    fix the velocity where the fighters were going off screen

--- src/components/Fighters/Fighter.jsx
+++ src/components/Fighters/Fighter.jsx
@@ -9,15 +9,28 @@ class Fighter {
     this.width = 50;
   }
   draw() {
-    this.ctx.fillStyle = 'red'
-    this.ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    this.ctx.fillStyle = "red";
+    this.ctx.fillRect(
+      this.position.x,
+      this.position.y,
+      this.width,
+      this.height
+    );
   }
 
   update() {
-    this.draw()
+    this.draw();
 
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
+
+    // stop the fighters from falling down of the canvas
+    if (this.position.y + this.height + this.velocity.y >= this.canvas.height) {
+      this.velocity.y = 0;
+    } else {
+      // Sprite is in the air, gets affected by gravity.
+      this.velocity.y += this.gravity; // Add gravity speed to the sprite every frame to slow it down.
+    }
   }
 }
 
